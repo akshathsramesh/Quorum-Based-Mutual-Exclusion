90437c86969350d40f94143ee531f53005e95bdd
akshathsramesh <akshath.sramesh@gmail.com>
2019-03-14 20:50:14 -0500

    First commit

--- src/Client.java
+++ src/Client.java
@@ -1,3 +1,15 @@
 public class Client {
 
+    public static void main(String[] args) {
+
+        if (args.length != 1)
+        {
+            System.out.println("Usage: java Client <client-number> ");
+            System.exit(1);
+        }
+
+        System.out.println("Starting the Client");
+
+    }
+
 }
