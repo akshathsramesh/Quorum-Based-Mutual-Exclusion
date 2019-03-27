f77ed3812e6f09e1b51656a6bcf5891354a418f7
akshathsramesh <akshath.sramesh@gmail.com>
2019-03-27 00:19:09 -0500

    Sever base function

--- src/Server.java
+++ src/Server.java
@@ -1,2 +1,152 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Server {
+
+    List<Node> allServerNodes = new LinkedList<>();
+    List<SocketForServer> serverSocketConnectionList = new LinkedList<>();
+    ServerSocket server;
+    String Id;
+    String port;
+    String ipAddress;
+    HashMap<String,SocketForServer> serverSocketConnectionHashMap = new HashMap<>();
+    HashMap<String, String> serverAndWorkFolder = new HashMap<>();
+
+    public List<Node> getAllServerNodes() {
+        return allServerNodes;
+    }
+
+    public class CommandParser extends Thread{
+
+        Server currentServer;
+
+        public CommandParser(Server currentServer){
+            this.currentServer = currentServer;
+        }
+
+        Pattern STATUS = Pattern.compile("^STATUS$");
+
+        int rx_cmd(Scanner cmd){
+            String cmd_in = null;
+            if (cmd.hasNext())
+                cmd_in = cmd.nextLine();
+            Matcher m_STATUS = STATUS.matcher(cmd_in);
+
+            if(m_STATUS.find()){
+                System.out.println("SERVER SOCKET STATUS:");
+                try {
+                    System.out.println("STATUS UP");
+                    System.out.println("SERVER ID: " + Id);
+                    System.out.println("SERVER IP ADDRESS: " + ipAddress);
+                    System.out.println("SERVER PORT: " + port);
+                }
+                catch (Exception e){
+                    System.out.println("SOMETHING WENT WRONG IN TERMINAL COMMAND PROCESSOR");
+                }
+
+            }
+
+            return 1;
+        }
+
+        public void run() {
+            System.out.println("Enter commands to set-up MESH Connection : START");
+            Scanner input = new Scanner(System.in);
+            while(rx_cmd(input) != 0) { }
+        }
+    }
+
+
+    public void setServerList(){
+        try {
+            BufferedReader br = new BufferedReader(new FileReader("config_server.txt"));
+            try {
+                StringBuilder sb = new StringBuilder();
+                String line = br.readLine();
+
+                while (line != null) {
+                    sb.append(line);
+                    List<String> parsed_server = Arrays.asList(line.split(","));
+                    Node n_server = new Node(parsed_server.get(0),parsed_server.get(1),parsed_server.get(2));
+                    this.getAllServerNodes().add(n_server);
+                    sb.append(System.lineSeparator());
+                    line = br.readLine();
+                }
+                String everything = sb.toString();
+                System.out.println(everything);
+                System.out.println(this.getAllServerNodes().size());
+
+            } finally {
+                br.close();
+            }
+        }
+        catch (Exception e) {
+        }
+    }
+
+    public void serverSocket(Integer serverId, Server currentServer){
+        try
+        {
+            server = new ServerSocket(Integer.valueOf(this.allServerNodes.get(serverId).port));
+            Id = Integer.toString(serverId);
+            port  = this.allServerNodes.get(serverId).port;
+            ipAddress = this.allServerNodes.get(serverId).ipAddress;
+            System.out.println("Server node running on port " + Integer.valueOf(this.allServerNodes.get(serverId).port) +"," + " use ctrl-C to end");
+            InetAddress myServerIp = InetAddress.getLocalHost();
+            String ip = myServerIp.getHostAddress();
+            String hostname = myServerIp.getHostName();
+            System.out.println("Your current Server IP address : " + ip);
+            System.out.println("Your current Server Hostname : " + hostname);
+        }
+        catch (IOException e)
+        {
+            System.out.println("Error creating socket");
+            System.exit(-1);
+        }
+
+        Server.CommandParser cmdpsr = new Server.CommandParser(currentServer);
+        cmdpsr.start();
+
+        Thread current_node = new Thread() {
+            public void run(){
+                while(true){
+                    try{
+                        Socket s = server.accept();
+                        SocketForServer socketForServer = new SocketForServer(s,Id, false,currentServer);
+                        serverSocketConnectionList.add(socketForServer);
+                        serverSocketConnectionHashMap.put(socketForServer.getRemote_id(),socketForServer);
+                    }
+                    catch(IOException e){ e.printStackTrace(); }
+                }
+            }
+        };
+
+        current_node.setDaemon(true);
+        current_node.start();
+    }
+
+
+
+    public static void main(String[] args) {
+
+
+        if (args.length != 1) {
+            System.out.println("Usage: java Server <server-number>");
+            System.exit(1);
+        }
+
+        System.out.println("Starting the Server");
+        Server server = new Server();
+        server.setServerList();
+        server.serverSocket(Integer.valueOf(args[0]),server);
+        System.out.println("Started the Server");
+    }
 }
---
+++ src/SocketForServer.java
@@ -0,0 +1,98 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class SocketForServer {
+    Socket otherClient;
+    String my_id;
+    String remote_id;
+    BufferedReader in;
+    PrintWriter out;
+    Boolean Initiator;
+    Server my_master;
+
+    public String getRemote_id() {
+        return remote_id;
+    }
+
+    public void setRemote_id(String remote_id) {
+        this.remote_id = remote_id;
+    }
+
+    public SocketForServer(Socket otherClient, String myId, Boolean isServer, Server my_master) {
+        this.otherClient = otherClient;
+        this.my_id = myId;
+        this.my_master = my_master;
+        try{
+            in = new BufferedReader(new InputStreamReader(this.otherClient.getInputStream()));
+            out = new PrintWriter(this.otherClient.getOutputStream(), true);
+        }
+        catch (Exception e){
+
+        }
+
+        try {
+            if(!isServer) {
+                out.println("SEND_CLIENT_ID");
+                System.out.println("SEND_CLIENT_ID request sent");
+                remote_id = in.readLine();
+                System.out.println("SEND_CLIENT_ID request response received with ID: " + remote_id);
+            }
+        }
+
+        catch (Exception e){
+
+        }
+        Thread read = new Thread(){
+            public void run(){
+                while(rx_cmd(in,out) != 0) { }
+            }
+        };
+        read.setDaemon(true); 	// terminate when main ends
+        read.start();
+    }
+
+
+    public int rx_cmd(BufferedReader cmd,PrintWriter out) {
+        try {
+            String cmd_in = cmd.readLine();
+            if (cmd_in.equals("WRITE_TEST")) {
+                System.out.println("Test write received from sender");
+            }
+        }
+        catch (Exception e){}
+        return 1;
+    }
+
+
+    public synchronized void publish() {
+        out.println("P");
+    }
+
+
+
+
+    public synchronized void sendWriteAcknowledge(String fileName){
+        System.out.println("Sending write ACK: " + fileName);
+        out.println("WRITE_TO_FILE_ACK");
+        out.println(fileName);
+    }
+
+
+    public synchronized void sendHostedFiles(String hostedFiles){
+        System.out.println("Sending hosted file info");
+        out.println("ENQUIRE_ACK");
+        out.println(hostedFiles);
+    }
+
+
+
+    public Socket getOtherClient() {
+        return otherClient;
+    }
+
+    public void setOtherClient(Socket otherClient) {
+        this.otherClient = otherClient;
+    }
+}
